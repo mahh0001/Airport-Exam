a4a44edc65c106dc39892cc72a55436b5a2b1650
Mathias Nielsen <mahh0001@edu.eal.dk>
2019-01-07 00:04:06 +0100

    Controller setup

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -10,17 +10,26 @@ namespace API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        //private readonly DATAFACTORY _datafactory;
+        //public CONTROLLERNAME(DATAFACTORY datafactory)
+        //{
+        //    _datafactory = datafactory;
+        //}
+
         // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        //[HttpGet]
+        //public ActionResult<IEnumerable<string>> Get()
+        //{
+        //List<Model> value = _datafactory.Table.ToList();
+        //return value;
+        //}
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
+            //var value = _datafactory.Table.First(x => x.Id == Id);
+            //return value;
             return "value";
         }
 
@@ -28,18 +37,51 @@ namespace API.Controllers
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            //_datafactory.Add(value);
+            //_datafactory.SaveChanges();
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+            if (!ModelState.IsValid)
+            {
+                BadRequest(ModelState);
+            }
+
+            //_db.Table.Update(value);
+            //_db.SaveChanges();
         }
+
+        //[HttpPatch("{id}")] 
+        //public void Patch([FromBody]JsonPatchDocument<Model> patch, int id) //patch is the model with updated values
+        //{
+        //    Model value = _db.Table.FirstOrDefault(x => x.Id == id);
+        //    patch.ApplyTo(value, ModelState);
+
+        //    if (!ModelState.IsValid)
+        //    {
+        //        BadRequest(ModelState);
+        //    }
+
+        //    var model = new
+        //    {
+        //        original = value,
+        //        patched = patch
+        //    };
+
+        //    _db.Table.Update(value);
+        //    _db.SaveChanges();
+        //}
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            //Model value = _db.Table.First(x => x.Id == id);
+            //_db.Table.Remove(value);
+            //_db.SaveChanges();
         }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -25,7 +25,8 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            //var connection = @"Server=(localdb)\mssqllocaldb;Database=GiftShop_TrialExam;Trusted_Connection=True";
+            //services.AddDbContext<***INSERTHERE***>(options => options.UseSqlServer(connection));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
